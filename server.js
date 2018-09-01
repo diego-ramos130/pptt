e6ac4e5b552ac92637a4f5848899efbfa11ee0e6
diego-ramos130 <diego.onethreezero@gmail.com>
2018-08-31 18:40:10 -0700

    big server ups, heroku deployment, and more views

--- server.js
+++ server.js
@@ -14,12 +14,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
-app.get('/', initializeHomePage)
+app.get('/', initializeHomePage);
+
 
 function initializeHomePage(req,res){
-  res.render('pages/main');
+    res.render('pages/main');
 }
 
+function throwError(response, err) {
+  console.error(err);
+  response.render('pages/sqlerror');
+}
+
+app.use(express.static(__dirname + '/public'));
+app.use('*', (req, res) => res.render('pages/error'));
 app.listen(PORT, () => console.log(`server hath started on port ${PORT}`));
 
 //database initialization
