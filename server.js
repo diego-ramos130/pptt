d92dfcfbba32cb9689db277b10fc9d739387ef30
Vince Masten <vmasten@me.com>
2018-09-04 13:06:54 -0700

    working on post method

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT;
 const app = express();
 const client = new pg.Client(process.env.DATABASE_URL);
 const superagent = require('superagent');
+const startOfString = 'https://api.github.com/repos/';
 client.connect();
 
 app.set('view engine', 'ejs');
@@ -29,11 +30,10 @@ function throwError(response, err) {
 
 function githubHit(req,res){
 // things that we can get from github api without an auth token: name of repo, url, open issues, issue name and link, date it's created, date of last push
-  let startOfString = 'https://api.github.com/repos/';
   let endOfString = `${req.body.user}/${req.body.repo}`;
   let conString = startOfString + endOfString;
   superagent.get(conString)
-    .then(data => {
+      .then(data => {
       console.log(data.body.html_url);
       console.log(data.body.name);
       console.log(data.body.open_issues);
@@ -47,11 +47,26 @@ function githubHit(req,res){
       console.log(datum.body.created_at);
       console.log(datum.body.updated_at);
     }) */
-    .catch(error => {
-      console.error(error);
-    });
+    // .catch(error => {
+    //   console.error(error);
+    // });
 }
 
+function githubPostToBase(req, res) {
+  let endOfString = `${req.body.user}/${req.body.repo}`;
+  let conString = startOfString + endOfString;
+  superagent.get(conString)
+    .then(data => {
+      console.log('hello');
+      let SQL = `INSERT INTO projects(name)
+      VALUES ($1);
+      `;
+      let values = [data.body.name];
+      console.log(data.body.name);
+      client.query(SQL, values);
+    });
+
+}
 
 app.use(express.static(__dirname + '/public'));
 app.use('*', (req, res) => res.render('pages/error'));
@@ -59,12 +74,14 @@ app.listen(PORT, () => console.log(`server hath started on port ${PORT}`));
 
 //database initialization
 
-/* let sampleRequest = {
+let sampleRequest = {
   body : {
     user : 'diego-ramos130',
     repo : 'pptt'
   }
-}; */
+}; 
 //console.log(sampleRequest);
 
-//githubHit(sampleRequest, 0);
+// githubHit(sampleRequest, 0);
+
+githubPostToBase(sampleRequest, 0);
