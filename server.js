1abee080b67670f0bb3b8d31ff4d6209ee4490d8
diego-ramos130 <diego.onethreezero@gmail.com>
2018-09-04 11:19:43 -0700

    finished first main get route from api

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
 app.get('/', initializeHomePage);
-
+app.get('/', githubHit); //put data into input fields here
+//app.post('', githubPostToBase); //take data out of input fields here and post to database. render accordingly 
 
 function initializeHomePage(req,res){
-    res.render('pages/main');
+  res.render('pages/main');
 }
 
 function throwError(response, err) {
@@ -26,9 +27,44 @@ function throwError(response, err) {
   response.render('pages/sqlerror');
 }
 
+function githubHit(req,res){
+// things that we can get from github api without an auth token: name of repo, url, open issues, issue name and link, date it's created, date of last push
+  let startOfString = 'https://api.github.com/repos/';
+  let endOfString = `${req.body.user}/${req.body.repo}`;
+  let conString = startOfString + endOfString;
+  superagent.get(conString)
+    .then(data => {
+      console.log(data.body.html_url);
+      console.log(data.body.name);
+      console.log(data.body.open_issues);
+    })
+  /* let issues = conString +'/issues/25';
+  console.log(issues);
+  superagent.get(issues)
+    .then(datum => {
+      console.log(datum.body.html_url);
+      console.log(datum.body.state);
+      console.log(datum.body.created_at);
+      console.log(datum.body.updated_at);
+    }) */
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+
 app.use(express.static(__dirname + '/public'));
 app.use('*', (req, res) => res.render('pages/error'));
 app.listen(PORT, () => console.log(`server hath started on port ${PORT}`));
 
 //database initialization
 
+/* let sampleRequest = {
+  body : {
+    user : 'diego-ramos130',
+    repo : 'pptt'
+  }
+}; */
+//console.log(sampleRequest);
+
+//githubHit(sampleRequest, 0);
